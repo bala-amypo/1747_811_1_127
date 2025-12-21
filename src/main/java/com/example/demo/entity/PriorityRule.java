3e6908c1047602c8db31852463e3eaaf5d2ffeae
bala-amypo <bala@amypo.in>
2025-12-21 06:15:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/PriorityRule.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+public class PriorityRule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ruleName;
+    private String description;
+    private Integer weight;
+    private boolean active = true;
+
+    @ManyToMany(mappedBy = "priorityRules")
+    private Set<Complaint> complaints;
+
+    
+    public Integer getWeight() { return weight; }
+    public boolean isActive() { return active; }
+}
